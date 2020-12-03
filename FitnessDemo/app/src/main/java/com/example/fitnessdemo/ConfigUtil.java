fec059df6a9ef222b20f4982f764e8b04775fd32
zhufangtian010727 <1263291610@qq.com>
2020-12-03 08:03:40 +0800

    motion

--- FitnessDemo/app/src/main/java/com/example/fitnessdemo/ConfigUtil.java
+++ FitnessDemo/app/src/main/java/com/example/fitnessdemo/ConfigUtil.java
@@ -1,13 +1,10 @@
 package com.example.fitnessdemo;
 
 public class ConfigUtil {
-<<<<<<< HEAD
     //王烁ip 192.168.43.165
-    public final static String SERVER_HOME = "http://192.168.43.165:8080/JSZS/";
-//    public final static String SERVER_HOME = "http://192.168.137.210:8080/WorkOut/";
-    public static String user_Name =null;
-=======
     public final static String SERVER_HOME = "http://10.7.89.163:8080/WorkOut/";
+//    public final static String SERVER_HOME = "http://192.168.137.210:8080/WorkOut/";
     public static String user_Name ="15701228161";
->>>>>>> 21f9f05ba9f43382c819e858d65d432946791131
+//    public final static String SERVER_HOME = "http://10.7.89.163:8080/WorkOut/";
+//    public static String user_Name ="15701228161";
 }
