c25ea0c3b9edfd822c50fdfc430b6e7af188da32
1848991298 <l>
2020-12-03 11:51:32 +0800

    ws

--- FitnessDemo/app/src/main/java/com/example/fitnessdemo/ws/MyInfo.java
+++ FitnessDemo/app/src/main/java/com/example/fitnessdemo/ws/MyInfo.java
@@ -1,5 +1,24 @@
 package com.example.fitnessdemo.ws;
 
-public class MyInfo
-{
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fitnessdemo.R;
+
+public class MyInfo extends Fragment {
+    private View root;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.ws_fragment_myinfo, container, false);
+        return root;
+    }
 }
