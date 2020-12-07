8d10241eb710497ed3b51e5a56d91ff649178b4d
Foreverxiaoning <2951932776@qq.com>
2020-12-07 23:50:33 +0800

    bobo

--- FitnessDemo/app/src/main/java/com/example/fitnessdemo/CLB/ClockActivity.java
+++ FitnessDemo/app/src/main/java/com/example/fitnessdemo/CLB/ClockActivity.java
@@ -3,8 +3,12 @@ package com.example.fitnessdemo.CLB;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,11 @@ import com.example.fitnessdemo.R;
 
 public class ClockActivity extends AppCompatActivity {
 //    private int timeUsedInSec =350000;
+    //自定义PopupWindow
+    private PopupWindow canclePop;
     private int timeUsedInSec = 0;
     private boolean paused  =false;
     private String timeUsed ;
-    private TextView minText;
-    private TextView secText;
-    private TextView misText;
     private TextView timeText;
     private Button btnStop;
     private Button btnBegin;
@@ -31,7 +34,6 @@ public class ClockActivity extends AppCompatActivity {
             switch (msg.what) {   //what的值为1而且paused为false时将Message送主线程
                 case 1:
                     if (!paused)
-
                     {
                         addTimeUsed();    //计时函数
                         updateClockUI();  //更新UI线程的数据
@@ -75,13 +77,48 @@ public class ClockActivity extends AppCompatActivity {
         mis=mis%100;
         return mis < 10 ? "0" + mis : String.valueOf(mis);
     }
+    /**
+     * 点击弹框
+     *
+     * @param
+     */
+    public void showPop() {
+        View contentView = null;
+        //设置contentView
+        if (canclePop == null) {
+            contentView = LayoutInflater.from(ClockActivity.this).inflate(R.layout.clb_pop_clock, null);
+            canclePop = new PopupWindow(contentView, RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.MATCH_PARENT, true);
+            canclePop.setFocusable(true);
+            canclePop.setOutsideTouchable(false);
+            canclePop.setClippingEnabled(false);
+        } else {
+            contentView = canclePop.getContentView();
+        }
+//        TextView tvBackHome = contentView.findViewById(R.id.tv_back_home);
+//        TextView tvCancle = contentView.findViewById(R.id.tv_cancle);
+//
+//        tvBackHome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //点击返回的逻辑
+//                canclePop.dismiss();
+//            }
+//        });
+//        tvCancle.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                canclePop.dismiss();
+//            }
+//        });
+        //显示PopupWindow
+        View rootview = LayoutInflater.from(ClockActivity.this).inflate(R.layout.activity_clock, null);
+        canclePop.showAtLocation(rootview, Gravity.BOTTOM, 0, 0);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock);
-//        minText = findViewById(R.id.minText);
-//        secText = findViewById(R.id.secText);
-//        misText = findViewById(R.id.misText);
         timeText = findViewById(R.id.timeText);
         btnStop = findViewById(R.id.btn_stop);
         btnBegin = findViewById(R.id.btn_begin);
@@ -93,6 +130,7 @@ public class ClockActivity extends AppCompatActivity {
                 message = uiHandler.obtainMessage();
                 message.what = 1;
                 uiHandler.sendMessage(message);
+                showPop();
 
             }
         });
