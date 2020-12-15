c06a0f01a433971e9c639fd60fc394804927e7ce
zhufangtian010727 <1263291610@qq.com>
2020-12-15 10:59:23 +0800

    up-zft

--- FitnessDemo/app/src/main/java/com/example/fitnessdemo/ConfigUtil.java
+++ FitnessDemo/app/src/main/java/com/example/fitnessdemo/ConfigUtil.java
@@ -1,6 +1,6 @@
 package com.example.fitnessdemo;
 
 public class ConfigUtil {
-    public final static String SERVER_HOME = "http://10.7.89.163:8080/WorkOut/";
-    public static String user_Name ="15158633056";
+    public final static String SERVER_HOME = "http://10.7.88.156:8080/WorkOut/";
+    public static String user_Name ="19832103582";
 }
