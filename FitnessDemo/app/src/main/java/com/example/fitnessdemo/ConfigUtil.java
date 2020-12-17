f761aac86dff721ad047dc36e58e163b4491ca6b
mengruiaa <985775625@qq.com>
2020-12-17 09:49:50 +0800

    mengrui1217

--- FitnessDemo/app/src/main/java/com/example/fitnessdemo/ConfigUtil.java
+++ FitnessDemo/app/src/main/java/com/example/fitnessdemo/ConfigUtil.java
@@ -3,9 +3,8 @@ package com.example.fitnessdemo;
 import android.graphics.Bitmap;
 
 public class ConfigUtil {
-    public final static String SERVER_HOME = "http://192.168.31.232:8080/WorkOut/";
-    public static String user_Name ="19832103582";
-    public static String bobo ="";
+    public final static String SERVER_HOME = "http://192.168.31.196:8080/JSZS/";
+    public static String user_Name ="19831125536";
     public static Bitmap user_Img = null;
 
 }
